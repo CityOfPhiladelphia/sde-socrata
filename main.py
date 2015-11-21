d655b07064303f82ca9f8fccce9d81ce140d05cb
timwis <tim@timwis.com>
2015-11-20 22:15:47 -0500

    Loop through datasets.yaml, push to socrata via datasync, rename geom fields to the_geom

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import arcpy
+import yaml
+import json
+import subprocess
 import unicodecsv as csv
 from slugify import slugify
 
 arcpy.env.workspace = 'C:/Users/tim/Desktop/ArcGIS/GIS_SDE_VIEWER.gdb'
 
-# Create SELECT statement
 def get_fields(table):
 	select = []
 	headers = []
@@ -13,18 +15,49 @@ def get_fields(table):
 	for field in fields:
 		if field.Type == 'Geometry':
 			select.append(field.Name + '@WKT')
+			headers.append('the_geom')
 		else:
 			select.append(field.Name)
-			
-		headers.append(slugify(field.Name, separator='_'))
-		
+			headers.append(slugify(field.Name, separator='_'))
+					
 	return select, headers
 
+# Load control file template
+with open('control.template.json') as control_json:
+	control_template = json.load(control_json)
 
-table = 'Healthy_corner_stores'
-select, headers = get_fields(table)
+# Fetch datasets config
+with open('datasets.yaml') as datasets_yaml:
+	datasets = yaml.load(datasets_yaml)
+	
+# For each dataset in datasets config
+for dataset in datasets:
+	control_file = control_template.copy()
+	
+	select, headers = get_fields(dataset['table'])
 
-with open('tmp/' + table + '.csv', 'wb') as f:
-	writer = csv.writer(f)
-	writer.writerow(headers)
-	writer.writerows(arcpy.da.SearchCursor(table, select))
+	with open('tmp/' + dataset['table'] + '.csv', 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(headers)
+		writer.writerows(arcpy.da.SearchCursor(dataset['table'], select))
+		
+		# Put clean headers into control file template
+		control_file['csv']['columns'] = headers
+	
+		# Write control file to disk
+		with open('tmp/control.' + dataset['table'] + '.json', 'w') as f:
+			json.dump(control_file, f)
+		
+	print('Pushing %s to %s' % (dataset['table'], dataset['socrata_id']))
+	
+	# Call DataSync w/csv and control file
+	subprocess.call([
+		'java',
+		'-jar', './bin/DataSync-1.6.jar',
+		'-c', 'config.json',
+		'-f', 'tmp/' + dataset['table'] + '.csv',
+		'-i', dataset['socrata_id'],
+		'-m', 'replace',
+		'-ph', 'true',
+		'-cf', 'tmp/control.' + dataset['table'] + '.json'
+	])
